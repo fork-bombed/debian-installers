9c36d7d515d5b3131ea639047d4573249c14813d
fork_bombed <ryanjmurphy2016@gmail.com>
2020-05-07 05:04:38 +0100

    Added Ghidra installation

--- installers/tools.sh
+++ installers/tools.sh
@@ -5,3 +5,18 @@ cd ~/pentesting
 # SecLists
 git clone https://github.com/danielmiessler/SecLists.git
 
+# Ghidra Installation
+ghidra_url="https://ghidra-sre.org/"
+ghidra_version="ghidra_9.1.2_PUBLIC_20200212.zip"
+location=${ghidra_version%_*}
+
+wget "${ghidra_url}${ghidra_version}"
+unzip $ghidra_version > /dev/null && rm $ghidra_version
+mv $location ghidra
+# Fix weird symlink problem with Ghidra's runner
+cat > ghidra/ghidraRun <<- EOM
+#!/usr/bin/env bash
+~/pentesting/ghidra/support/launch.sh bg Ghidra "" "" ghidra.GhidraRun "$@"
+EOM
+
+sudo ln -s ~/pentesting/ghidra/ghidraRun /usr/local/bin/ghidra
